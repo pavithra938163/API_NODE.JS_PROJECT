ae527b1d37b677adc4a4c0fa5e4ee1031c38a3e8
pavithra938163 <pavithra938163@gmail.com>
2026-04-04 15:41:05 +0530

    Create error.middleware.js

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,11 @@
+function errorMiddleware(err, req, res, _next) {
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || 'Internal server error',
+    path: req.originalUrl
+  });
+}
+
+module.exports = errorMiddleware;
