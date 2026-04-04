a08e95dae190f53b8eb1f255442ac7029c154eca
pavithra938163 <pavithra938163@gmail.com>
2026-04-04 15:42:26 +0530

    Create notFound.middleware.js

---
+++ src/middlewares/notFound.middleware.js
@@ -0,0 +1,8 @@
+function notFoundMiddleware(req, res) {
+  res.status(404).json({
+    success: false,
+    message: `Route not found: ${req.originalUrl}`
+  });
+}
+
+module.exports = notFoundMiddleware;
